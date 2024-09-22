bbff070ae0b4e32327554eecf5317768bf3997da
Neluni Priyangika <96222@fhss.sjp.ac.lk>
2024-09-22 11:12:21 +0530

    add the LogOut button to the both advisor and seeker home pages

--- client/src/components/advisorHome/AdvisorHome.jsx
+++ client/src/components/advisorHome/AdvisorHome.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useLogout } from '../logOut/LogOut';
 
 const AdvisorHome = () => {
-  return <h1>Welcome to Advisor Home</h1>;
+  const logout = useLogout();
+
+  return (
+    <div>
+      <h1>Welcome to Advisor Home</h1>
+      <button onClick={logout}>Log Out</button>
+    </div>
+  );
 };
 
 export default AdvisorHome;
