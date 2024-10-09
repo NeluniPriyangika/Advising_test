abea2d8e005401d2e32bf63095af5f0608f80002
Neluni Priyangika <96222@fhss.sjp.ac.lk>
2024-10-09 15:45:44 +0530

    add CSS to the SeekerUpdateProfile component

--- client/src/components/seekerUpdateProfile/SeekerUpdateProfile.jsx
+++ client/src/components/seekerUpdateProfile/SeekerUpdateProfile.jsx
@@ -5,8 +5,50 @@ import 'react-datepicker/dist/react-datepicker.css';
 import DatePicker from 'react-datepicker';
 import Navbar from '../navbar/Navbar';
 import Footer from '../footer/Footer';
+import Seeker1 from '../../assets/seeker1.png';
+import Seeker2 from '../../assets/seeker2.png';
+import Seeker3 from '../../assets/seeker3.png';
+import ReadOnlyRating from '../readOnlyRating/ReadOnlyRating';
+
+
+const SeekerReviews = (props) => (
+  <div className="seekerReviews">
+    <div className='seekerReviews-content1'>
+      <img src={ props.imgUrl } 
+      alt={ props.alt || 'Image' } />
+    </div>
+    <div className="seekerReviews-content2">
+      <h2>{ props.title }</h2>
+      <div className='seekerReviews-rating'> {props.homeRating}</div>
+      <p className='seekerReviews-desc'>"{props.subtitle}</p>
+    </div>  
+  </div>
+);
+
+const SeekerReviewsContainer = (props) => (
+  <div className="seekerReviews-container">
+    {
+      props.reviews.map((review) => (
+        <SeekerReviews title={ review.title }
+          personalDes={ review.personalDes }
+          imgUrl={ review.imgUrl }
+          timeText = {review.timeText} 
+          homeRating = {review.homeRating}
+          subtitle = {review.subtitle}/>
+      ))
+    }
+  </div>
+);
 
 const SeekerUpdateProfile = () => {
+
+  const seekerReviews = [
+    {id: 1, homeRating:<ReadOnlyRating/>, title: 'Serenity Stone',subtitle:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`, personalDes:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", content: 'Clark Kent',timeText:'5 minutes for  $1', imgUrl: 'https://unsplash.it/200/200'},
+    {id: 2, homeRating:<ReadOnlyRating/>, title: 'Michel Jackson',subtitle:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`, personalDes:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", content: 'Bruce Wayne',timeText:'5 minutes for  $1', imgUrl: 'https://unsplash.it/201/200'},
+    {id: 3, homeRating:<ReadOnlyRating/>, title: 'Serenity Stone',subtitle:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`, personalDes:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", content: 'Peter Parker',timeText:'5 minutes for  $1', imgUrl: 'https://unsplash.it/200/201'},
+    {id: 3, homeRating:<ReadOnlyRating/>, title: 'Serenity Stone',subtitle:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`, personalDes:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", content: 'Peter Parker',timeText:'5 minutes for  $1', imgUrl: 'https://unsplash.it/200/201'},
+  ]
+
   const [profileData, setProfileData] = useState({
     fullName: '',
     address: '',
@@ -101,83 +143,111 @@ const SeekerUpdateProfile = () => {
   return (
     <div className='seeker-updateprofile-main'>
       <Navbar/>
-      <div></div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="fullName"
-          value={profileData.fullName}
-          onChange={handleInputChange}
-          placeholder="Full Name"
-          required
-        />
-        <input
-          type="text"
-          name="address"
-          value={profileData.address}
-          onChange={handleInputChange}
-          placeholder="Address"
-          required
-        />
- 
-        <input
-            type="tel"
-            name="phoneNumber"
-            value={profileData.phoneNumber}
-            onChange={handleInputChange}
-            placeholder="Phone Number"
-            required
-        />
+      <div className='seeker-updateprofile-container'>
+        <div className='seeker-updateprofile-leftcontainer'>
 
-        <h4>Add your registerd email</h4>
+            <h3 className='seeker-updateprofile-leftcontainer-header'>Seeker Reviews</h3>
 
-        <input
-          type="email"
-          name="email"
-          value={profileData.email}
-          onChange={handleInputChange}
-          placeholder="Email"
-          required
-        />
+            <div className='seeker-rewiews-container'>
+              <div className='seeker-rewiews-content'>
+                <SeekerReviewsContainer reviews={ seekerReviews } />
+              </div>
+
+            </div>
+        </div>
+        <div className='seeker-updateprofile-form-container'>
+          <form className='seeker-updateprofile-form' onSubmit={handleSubmit}>
+            
+            <input
+              type="text"
+              name="fullName"
+              value={profileData.fullName}
+              onChange={handleInputChange}
+              placeholder="Full Name"
+              required
+            />
+
+            <input
+              type="text"
+              name="address"
+              value={profileData.address}
+              onChange={handleInputChange}
+              placeholder="Address"
+              required
+            />
+    
+            <input
+                type="tel"
+                name="phoneNumber"
+                value={profileData.phoneNumber}
+                onChange={handleInputChange}
+                placeholder="Phone Number"
+                required
+            />
+
+            <h4>Add Your Registerd Email</h4>
+
+            <input
+              type="email"
+              name="email"
+              value={profileData.email}
+              onChange={handleInputChange}
+              placeholder="Email"
+              required
+            />
 
 
-        <DatePicker
-          selected={profileData.birthday}
-          onChange={handleDateChange}
-          dateFormat="yyyy-MM-dd"
-          placeholderText="Select Birthday"
-          showYearDropdown
-          showMonthDropdown
-          scrollableYearDropdown
-          required
-        />
+            <DatePicker
+              className='datepicker'
+              selected={profileData.birthday}
+              onChange={handleDateChange}
+              dateFormat="yyyy-MM-dd"
+              placeholderText="Select Birthday"
+              showYearDropdown
+              showMonthDropdown
+              scrollableYearDropdown
+              required
+            />
 
-        <textarea 
-            className='seeker-profile-update-description'
-            name="description"
-            value={profileData.description}
-            onChange={handleInputChange}
-            placeholder="Short Bio or Description"
-        />
+            <textarea 
+                className='seeker-profile-update-description'
+                name="description"
+                value={profileData.description}
+                onChange={handleInputChange}
+                placeholder="Short Bio or Description"
+            />
 
-        <input
-            type="text"
-            name="language"
-            value={profileData.language}
-            onChange={handleInputChange}
-            placeholder="Language"
-            required
-        />
+            <input
+                type="text"
+                name="language"
+                value={profileData.language}
+                onChange={handleInputChange}
+                placeholder="Language"
+                required
+            />
 
-        <input
-          type="file"
-          accept="image/*"
-          onChange={handlePhotoChange}
-          required
-        />
+            <h4>Add your Profile Picture</h4>
 
-        <button type="submit">Update Profile</button>
-      </form>
+            <label htmlFor="">
+              <input
+                type="file"
+                accept="image/*"
+                onChange={handlePhotoChange}
+                required
+              />
+            </label>
+
+            <button className='seeker-profile-update-button' type="submit">Update Profile</button>
+          </form>
+
+        </div>
+  
+        <div className='seeker-updateprofile-rightcontainer'>
+          right
+        </div>
+
+      </div>
+      
       <Footer/>
     </div>
   );
